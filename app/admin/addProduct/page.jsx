cde9792e08647d521cfdf375e3daf6a1038eb7d2
DineshKuna <kunadinesh.work@gmail.com>
2025-09-09 18:39:07 +0530

    addproduct page work in progress

---
+++ app/admin/addProduct/page.jsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { assets } from '@/Assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <form className='pt-5 px-5 sm:pt-12 sm:pl-16'>
+        <p className='text-xl'>Upload thumbnail</p>
+        <label htmlFor="">
+            <Image src={assets.upload_area} width={140} height={70} alt=''/>
+        </label>
+        <input type="file" id='image' />
+         
+    </form>
+
+
+
+    </>
+  )
+}
+
+export default page
